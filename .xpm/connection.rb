d3966924a5891001b2cc3bdb0ba0c52577ea99b3
David Guyon <david@guyon.me>
2015-03-06 16:41:44 +0100

    Add: .xpm/connection.rb file

---
+++ .xpm/connection.rb
@@ -0,0 +1,7 @@
+set :g5k_user, "dguyon"
+# gateway
+set :gateway, "#{g5k_user}@access.grid5000.fr"
+# # This key will used to access the gateway and nodes
+ssh_options[:keys]= [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+# # This key will be installed on nodes
+set :ssh_public,  File.join(ENV["HOME"], ".ssh", "id_rsa.pub")
